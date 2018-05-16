3a07db86b2e35b558211d9af876b5d0230134acf
chaoui mohammed <chaoui.90@hotmail.es>
2018-05-16 16:29:42 +0000

    CM:Project Creation Changed to Cross plateform
    In this commit you'll find the login page created as well as the the logic behind it.
    The class constants contain the IP adress used by my host Api change it to your own.

---
+++ MeteoMobileApp/MeteoMobileApp/MeteoMobileApp/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeteoMobileApp
+{
+    public class Constants
+    {
+        public static string BaseApiAddress => "https://192.168.1.34:44320/";
+    }
+}
---
+++ MeteoMobileApp/MeteoMobileApp/MeteoMobileApp/Services/ApiServices.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using MeteoMobileApp.Helpers;
+
+
+namespace MeteoMobileApp.Services
+{
+    class ApiServices
+    {
+        public async Task<string> LoginAsync(string userName, string password)
+        {
+            var keyValues = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("username", userName),
+                new KeyValuePair<string, string>("password", password),
+                new KeyValuePair<string, string>("grant_type", "password")
+            };
+
+            var request = new HttpRequestMessage(
+                HttpMethod.Post, Constants.BaseApiAddress + "token");
+
+            request.Content = new FormUrlEncodedContent(keyValues);
+
+            var client = new HttpClient();
+            var response = await client.SendAsync(request);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            JObject jwtDynamic = JsonConvert.DeserializeObject<dynamic>(content);
+
+            var accessTokenExpiration = jwtDynamic.Value<DateTime>(".expires");
+            var accessToken = jwtDynamic.Value<string>("access_token");
+
+            Settings.AccessTokenExpirationDate = accessTokenExpiration;
+
+            System.Diagnostics.Debug.WriteLine(accessTokenExpiration);
+
+            System.Diagnostics.Debug.WriteLine(content);
+
+            return accessToken;
+        }
+    }
+}
