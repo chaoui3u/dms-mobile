afcd0552337990baacbda0d38c27da2915920aeb
chaoui mohammed <chaoui.90@hotmail.es>
2018-05-22 14:39:46 +0000

    CM: Test Charts Created

--- MeteoMobile/MeteoMobile/MeteoMobile/Views/StatisticsTabbedPageDetail.xaml.cs
+++ MeteoMobile/MeteoMobile/MeteoMobile/Views/StatisticsTabbedPageDetail.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microcharts;
+using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +8,88 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Entry = Microcharts.Entry;
 
 namespace MeteoMobile.Views
 {
+   
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StatisticsTabbedPageDetail : TabbedPage
     {
+        List<Entry> entriesPage1 = new List<Entry>
+    {
+        new Entry(200)
+        {
+            Color = SKColor.Parse("#FF1493"),
+            Label = "Data Sample 1",
+            ValueLabel="200 test"
+        },
+        new Entry(150)
+        {
+            Color = SKColor.Parse("#00BFFF"),
+            Label = "Data Sample 2",
+            ValueLabel="150 test"
+        },
+        new Entry(300)
+        {
+            Color = SKColor.Parse("#00CED1"),
+            Label = "Data Sample 3",
+            ValueLabel="300 test"
+        }
+    };
+
+        List<Entry> entriesPage2 = new List<Entry>
+    {
+        new Entry(200)
+        {
+            Color = SKColor.Parse("#FF1493"),
+            Label = "Data Sample 1",
+            ValueLabel="200 test"
+        },
+        new Entry(150)
+        {
+            Color = SKColor.Parse("#00BFFF"),
+            Label = "Data Sample 2",
+            ValueLabel="150 test"
+        },
+        new Entry(300)
+        {
+            Color = SKColor.Parse("#00CED1"),
+            Label = "Data Sample 3",
+            ValueLabel="300 test"
+        }
+    };
+
+        List<Entry> entriesPage3 = new List<Entry>
+    {
+        new Entry(200)
+        {
+            Color = SKColor.Parse("#FF1493"),
+            Label = "Data Sample 1",
+            ValueLabel="200 test"
+        },
+        new Entry(150)
+        {
+            Color = SKColor.Parse("#00BFFF"),
+            Label = "Data Sample 2",
+            ValueLabel="150 test"
+        },
+        new Entry(300)
+        {
+            Color = SKColor.Parse("#00CED1"),
+            Label = "Data Sample 3",
+            ValueLabel="300 test"
+        }
+    };
+
+
         public StatisticsTabbedPageDetail ()
         {
             InitializeComponent();
+
+            Chart1.Chart = new PointChart { Entries = entriesPage1, };
+            Chart2.Chart = new BarChart { Entries = entriesPage2 };
+            Chart3.Chart = new LineChart { Entries = entriesPage3 };
         }
     }
 }
